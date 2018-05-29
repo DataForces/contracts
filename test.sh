5117918f71f8fb38e8616259366680cf1bfc8307
go_hiroaki <guangmingwu2010@gmail.com>
2018-05-30 02:07:24 +0900

    update values

--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
-# test on function evenAirdrop ->[addresses, int_unit]
-["0x32bdF9dE3184869fF9dEDBb50653dD826403b885","0x6EF6FEda2CBeF4f4C27102D95B3fD56225c1E3af"], 5000
-# test on function unevenAirdrop ->[addresses, decimal_unit]
-["0x32bdF9dE3184869fF9dEDBb50653dD826403b885","0x6EF6FEda2CBeF4f4C27102D95B3fD56225c1E3af"], [10000, 5000]
+# test on function evenAirdrop ->[addresses, value]
+["0x32bdF9dE3184869fF9dEDBb50653dD826403b885","0x6EF6FEda2CBeF4f4C27102D95B3fD56225c1E3af"], 5000000000000000000000
+# test on function unevenAirdrop ->[addresses, value]
+["0x32bdF9dE3184869fF9dEDBb50653dD826403b885","0x6EF6FEda2CBeF4f4C27102D95B3fD56225c1E3af"], [10000000000000000000000, 5000000000000000000000]
 # test on function freezeAccounts
 ["0x32bdF9dE3184869fF9dEDBb50653dD826403b885","0x6EF6FEda2CBeF4f4C27102D95B3fD56225c1E3af"], true
 ["0x32bdF9dE3184869fF9dEDBb50653dD826403b885","0x6EF6FEda2CBeF4f4C27102D95B3fD56225c1E3af"], false
-# test on function transfer ->[address, decimal_unit]
+# test on function transfer ->[address, value]
 "0x32bdF9dE3184869fF9dEDBb50653dD826403b885", 1000000000000000000
-# test on function mint -> [address, int_unit]
+# test on function mint -> [address, amount]
 "0x32bdF9dE3184869fF9dEDBb50653dD826403b885", 100000000
